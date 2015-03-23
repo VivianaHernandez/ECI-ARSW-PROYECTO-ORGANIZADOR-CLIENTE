b644882cefbdc927d7e42c0cd4ac2578ee562b95
viviana <viviana@Viviana-PC>
2015-03-22 19:14:25 -0500

    Calendario

---
+++ src/main/java/edu/eci/arsw/Cliente/CalendarioCaptureException.java
@@ -0,0 +1,15 @@
+package edu.eci.arsw.Cliente;
+
+public class CalendarioCaptureException extends Exception {
+
+	public CalendarioCaptureException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CalendarioCaptureException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/edu/eci/arsw/Cliente/DocumentoViewer.java
@@ -0,0 +1,52 @@
+package edu.eci.arsw.Cliente;
+
+import edu.eci.arsw.CalendarioComun.TColaborativa;
+import java.awt.*;
+import javax.swing.*;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DocumentoViewer {
+
+    static CalendarioCaptureStub calendarioCaptureStub;
+    static Documento d;
+    static TColaborativa tc;
+   
+            
+    public static void main(String[] args) throws java.io.IOException, AWTException, InterruptedException, CalendarioCaptureException, RemoteException, AccessException, NotBoundException, BadLocationException {
+      
+        ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
+        final CalendarioCaptureStub calendarioCaptureStub = (CalendarioCaptureStub)ac.getBean("calendarioCaptureStub");
+        d=new Documento(calendarioCaptureStub);
+        
+        tc=calendarioCaptureStub.getTareaColaborativa();
+        
+        System.out.println("*******************************************");
+        
+        System.out.println("Nombre: "+tc.getNombre());
+        System.out.println("Fecha: "+tc.getFecha().getDia());
+        System.out.println("descripcion: "+tc.getDescripcion());
+        
+    }
+    
+    public static CalendarioCaptureStub getProxy(String ip, int puerto, String nombreObjeto) throws AccessException, RemoteException, NotBoundException {
+
+        Registry reg = LocateRegistry.getRegistry(ip, puerto);
+        CalendarioCaptureStub objeto = (CalendarioCaptureStub) reg.lookup(nombreObjeto);
+        return objeto;
+    }
+
+    public void setDocumentoCaptureStub(CalendarioCaptureStub calendarioCaptureStub) {
+        this.calendarioCaptureStub = calendarioCaptureStub;
+    }
+}
