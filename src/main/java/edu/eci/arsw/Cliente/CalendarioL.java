92980a1cc3ecfa54fceebc8ad9e1df07a01bdfb3
VivianaHernandez <vivianahg13@gmail.com>
2015-03-27 14:01:20 -0500

    Calendario logico 3

--- src/main/java/edu/eci/arsw/Cliente/CalendarioL.java
+++ src/main/java/edu/eci/arsw/Cliente/CalendarioL.java
@@ -8,17 +8,48 @@ package edu.eci.arsw.Cliente;
 import edu.eci.arsw.CalendarioComun.Fecha;
 import edu.eci.arsw.CalendarioComun.TColaborativa;
 import edu.eci.arsw.CalendarioComun.TInformativa;
+import java.util.ArrayList;
 
 /**
  *
  * @author Torres
  */
 public class CalendarioL {
-   private Fecha fecha;
     
-    CalendarioL(Fecha fecha, TColaborativa colaborativa, TInformativa informativa) {
-        
+   private ArrayList<TInformativa> tareasInformativas=new ArrayList<TInformativa>();
+   private ArrayList<TColaborativa> tareasColaborativas=new ArrayList<TColaborativa>();
+    
+    CalendarioL() {
+      
     }
+    
+    public void agregarTareaInformativa(TInformativa ti)
+    {
+    tareasInformativas.add(ti);
+    }
+    
+    public void agregarTareaColaborativa(TColaborativa tc)
+    {
+    tareasColaborativas.add(tc);
+    }
+    
+    public ArrayList<TInformativa> getTareasInformativas() {
+        return tareasInformativas;
+    }
+
+    public void setTareasInformativas(ArrayList<TInformativa> tareasInformativas) {
+        this.tareasInformativas = tareasInformativas;
+    }
+
+    public ArrayList<TColaborativa> getTareasColaborativas() {
+        return tareasColaborativas;
+    }
+
+    public void setTareasColaborativas(ArrayList<TColaborativa> tareasColaborativas) {
+        this.tareasColaborativas = tareasColaborativas;
+    }
+
+    
     
     
 }
