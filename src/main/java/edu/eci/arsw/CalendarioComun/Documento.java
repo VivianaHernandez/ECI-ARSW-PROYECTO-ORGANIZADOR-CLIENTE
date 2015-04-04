2fb30f11082ac0bcb4cb90652a3244341f5beab0
viviana <viviana@Viviana-PC>
2015-04-03 21:49:41 -0500

    Doc

--- src/main/java/edu/eci/arsw/CalendarioComun/Documento.java
+++ src/main/java/edu/eci/arsw/CalendarioComun/Documento.java
@@ -5,87 +5,131 @@
  */
 package edu.eci.arsw.CalendarioComun;
 
+import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.text.BadLocationException;
 
-public class Documento {
+public class Documento implements Serializable{
 
     static String palabras="";
     static boolean b=true;
     static String text;
     static String nuevo="";
     static int pos;
+    public boolean estado=true;
+    static CalendarioCaptureStub calendarioCaptureStub;
+    public int band=0;
     
-    public Documento(final CalendarioCaptureStub calendarioCaptureStub) throws BadLocationException
-    {
-         initComponents();
-        jf.setSize(600,400);
+    public Documento(final CalendarioCaptureStub calendarioCaptureStub) throws BadLocationException, CalendarioCaptureException {
+        this.calendarioCaptureStub = calendarioCaptureStub;
+        initComponents();
+        jf.setSize(600, 400);
         jf.setTitle("Aplicacion del cliente");
         jf.setVisible(true);
-        
-        textArea.addCaretListener(new CaretListener(){
-        
-                @Override
-                public void caretUpdate(CaretEvent e){
-               
-               int pos=e.getDot(),longi=1;
-               text=textArea.getText();
-              
-                    try {
-                        nuevo = textArea.getText(pos-1,longi);
-                       
-                    } catch (BadLocationException ex) {
-                        Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
-                    } 
-                    try {
-                        calendarioCaptureStub.setTexto(pos,nuevo);
-                    } catch (CalendarioCaptureException ex) {
-                        Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+
+     
+        textArea.addCaretListener(new CaretListener() {
+
+            @Override
+            public void caretUpdate(CaretEvent e) {
+            band++;
+            System.out.println("Banderz: "+band);
+                int pos = e.getDot(), longi = 1;
+                text = textArea.getText();
+                        
+                try {
+                    nuevo = textArea.getText(pos-1, longi);
+                    System.out.println("Nuevo: " +nuevo+".");
+                    System.out.println("Pos: " +pos);
+                } catch (BadLocationException ex) {
+                    Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
                 }
+
+                try {
+                    calendarioCaptureStub.setTexto(pos, nuevo);
+                } catch (CalendarioCaptureException ex) {
+                    Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        
+                        System.out.println("entro´++++++++++++++++");
+                        try {
+                            calendarioCaptureStub.notificarCambio(true);
+                            palabras = calendarioCaptureStub.getTexto();
+                            System.out.println("Tamaño: "+palabras.length());
+                            setTexto(palabras);
+                            
+                            
+                        
+                        } catch (CalendarioCaptureException ex) {
+                            Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (BadLocationException ex) {
+                            Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
+                        } 
+                    }
+                });
+
+            }
         });
-       /* try {
-                     
-            while(b==true){
-              
-            palabras=calendarioCaptureStub.getTexto();
-                
-                        setTexto(palabras);
-                        jf.repaint();
-            }
-                    } catch (CalendarioCaptureException ex) {
-                        Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
-                    }*/
+       
+         System.out.println("Hello afuera "+estado);
+    while(estado==true){
+      palabras = calendarioCaptureStub.getTexto();
+    System.out.println("Hello "+estado);
+      setTexto(palabras);  
+      estado=calendarioCaptureStub.estadoCambio();
     }
+      
+    }
+
+
     
-    
-    public static void setTexto(String texto) throws BadLocationException
+    public static void setTexto(String texto) throws BadLocationException, CalendarioCaptureException
     {
+ System.out.println("Etro al metodo=)=)=)=)=)=)=)=)=)=)=)=)=)=)");
+ int tams=texto.length();
+ 
+ System.out.println("Tamaño s "+texto.length());
+ 
     String s=texto;
-     int uu=1,i;
+     int uu=1;
      char w;
-  System.out.println("llega a set Textoooooooooooooooooo");
-        for (i = 0; i < s.length(); i++) {
-            w = s.charAt(i);
-            String ww = w + "";
-            String a = textArea.getText(i, uu);
-System.out.println("llega a if equalsssssssssss");
-            if (!ww.equals(a)) {
-                
+     String ww;
+  System.out.println("SSSSS: ."+s+".");
+  if(s.length()>0){
+   for (int i = 1; i < tams-1; i++) {
+            
+       System.out.println("Aquise traba....");
+       w = s.charAt(i);
+            ww = w + "";
+            
+             System.out.println("\nww:"+w+".. En"+i);
+            
+            String a = textArea.getText(i-1, uu);
+            System.out.println("AA:"+a+".. En"+i);
+            if ((!ww.equals(a))&&(!ww.contains(" "))) {
+               
+                System.out.println("va a insertar:"+ww+".. En"+i);
                 textArea.insert(ww, i);
+                 jf.repaint();   
             }
-        }
-   if(s.length()>0){
-     for(i=s.length();i<100;i++)
-     {
-     textArea.insert(" ", i);
-     }}
+   }
     
+   
+ 
+   
+  
+  } 
+     calendarioCaptureStub.notificarCambio(false); 
     }
+    
  
     private static void saveMenuItemActionPerformed(java.awt.event.ActionEvent evt) {                                             
           try {
@@ -102,6 +146,11 @@ System.out.println("llega a if equalsssssssssss");
             Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
         }
     } 
+    
+    public void setVisibles(boolean estado)
+    {
+    jf.setVisible(estado);
+    }
     
     public static void initComponents() {
 
@@ -126,7 +175,7 @@ System.out.println("llega a if equalsssssssssss");
 
         jf.getContentPane().add(textjsp, java.awt.BorderLayout.CENTER);
 
-        for(int i=0;i<100;i++){
+        for(int i=1;i<100;i++){
        textArea.insert(" ",i);}
                 
         jMenu1.setText("File");
